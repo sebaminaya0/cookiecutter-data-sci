11d065e80e16ce39b5884c38629bc1dcf975a729
sebaminaya0 <sebastian.minaya1998@gmail.com>
2023-09-23 09:56:22 -0500

    Updated to automate conda environment creation + notebook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,7 +5,7 @@ MESSAGE_COLOR = '\033[94m'
 RESET_COLOR = '\033[0m'
 
 print(f'{MESSAGE_COLOR}Installing dependencies...{RESET_COLOR}')
-print(f'Initializing git repo...{RESET_COLOR}')
+print(f'{MESSAGE_COLOR}Initializing git repo...{RESET_COLOR}')
 
 username = '{{ cookiecutter.github_username }}'
 email = '{{ cookiecutter.email }}'
@@ -15,5 +15,10 @@ subprocess.call(['git', 'add', '.'])
 subprocess.call(['git', 'congif', 'user.name', f'{username}'])
 subprocess.call(['git', 'congif', 'email.name', f'{email}'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+
+print(f'{MESSAGE_COLOR}Creating conda environment...{RESET_COLOR}')
+
+subprocess.call(['conda', 'env', 'create', '-f', 'environment.yml'])
+subprocess.call(['conda', 'activate', '{{ cookiecutter.project_slug }}'])
 
 print(f'{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_COLOR}')
